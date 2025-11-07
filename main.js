6351280f8139b8a1934007b714765d1c4b9fdc8b
Teste <silly>
2025-11-07 20:43:53 -0300

    YEAH YEAH KEEP GOING MEOW MEOW

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-player = {
-    version: "alpha0.01",
+let player = {
+    version: "alpha0.03",
     m_number: 10,
     m_values: [0,0,0,0],
-    m_valuebuy: [0,0,0,0],
-    m_extravalues: 0
+    m_valuebuys: [0,0,0,0]
 }
+const tickspersecond = 20
+const constants = {
+    m_levelmult: 2
+}
+setInterval(() => {
+    for(let i = 0; i < player.m_values.length-1; i++){
+       player.m_values[i] += player.m_values[i+1]/tickspersecond * 2**player.m_valuebuys[i+1]
+    }
+    player.m_number += player.m_values[1]/tickspersecond * 2**player.m_valuebuys[1]
+}, 1000/tickspersecond);
