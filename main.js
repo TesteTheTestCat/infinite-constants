a98398b78b0b105d92fcb7889b674db31bebf148
Teste <silly>
2025-11-08 14:19:15 -0300

    meow mrrp purrr meow

--- main.js
+++ main.js
@@ -7,10 +7,24 @@ let player = {
 }
 const gel = (name) => document.getElementById(name)
 const tickspersecond = 20
+function makevalues(list){
+   let kije = ""
+   for(let i = 0; i < list.length; i++){
+     kije += `
+        <tr>
+          <td>Value ${i}</td>
+          <td>Level 0/10 (*1)</td>
+          <td>${format(list[i],4)}</td>
+          <td><button>${format(new Decimal(10).times(i))}</button></td>
+        </tr>`
+   }
+   return kije
+}
 setInterval(() => {
     for(let i = 0; i < player.m_values.length-1; i++){
        player.m_values[i] = player.m_values[i].add(player.m_values[i+1].divide(tickspersecond).times(new Decimal(2).pow(player.m_valuebuys[i+1])))
     }
     player.m_number = player.m_number.add(player.m_values[0].divide(tickspersecond).times(new Decimal(2).pow(player.m_valuebuys[0])))
-    gel("m_number").textContent = format(player.m_number)
+    gel("m_number").textContent = format(player.m_number,6)
+    gel("m_values").textContent = makevalues(player.m_values)
 }, 1000/tickspersecond);
