81b9ee9645fe632964826a94b2b06d34ad3a4b19
Teste <silly>
2025-11-08 15:20:14 -0300

    more testing~ :3

--- main.js
+++ main.js
@@ -1,22 +1,23 @@
-import {format} from "./formatting.js";
+import {format, formatWhole} from "./formatting.js";
 let player = {
     version: "alpha0.03",
     m_number: new Decimal(10),
-    m_values: [new Decimal(0),new Decimal(0),new Decimal(0),new Decimal(1)],
+    m_values: [new Decimal(0),new Decimal(0),new Decimal(0),new Decimal(0)],
     m_valuebuys: [new Decimal(0),new Decimal(0),new Decimal(0),new Decimal(0)]
 }
 const gel = (name) => document.getElementById(name)
 const tickspersecond = 20
-function makevalues(list){
+function makevalues(list,meow){
    let kije = ""
    for(let i = 0; i < list.length; i++){
      kije += `
         <tr style="width: 100%">
           <td>Value ${i+1}</td>
-          <td>Level 0/10 (*1)</td>
+          <td>Level ${formatWhole(meow[i])}/10 (*${new Decimal(2).pow(meow[i])})</td>
           <td>${format(list[i],4)}</td>
-          <td><button>${format(new Decimal(10).pow(i+1))}</button></td>
+          <td><button id="m_buybutton${i}">${format(new Decimal(10).pow(i+1))}</button></td>
         </tr><br>`
+     gel(`m_buybutton${i}`).onclick = () => {player.m_values = player.m_values.add(1); player.m_valuebuys = player.m_valuebuys.add(1)}
    }
    return kije
 }
@@ -26,5 +27,5 @@ setInterval(() => {
     }
     player.m_number = player.m_number.add(player.m_values[0].divide(tickspersecond).times(new Decimal(2).pow(player.m_valuebuys[0])))
     gel("m_number").textContent = format(player.m_number,6)
-    gel("m_values").innerHTML = makevalues(player.m_values)
+    gel("m_values").innerHTML = makevalues(player.m_values,player.m_valuebuys)
 }, 1000/tickspersecond);
