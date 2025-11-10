5b36730945370bcc81c79cc26bee17b06c24bbfe
Teste <silly>
2025-11-10 19:14:49 -0300

    maybe this'll work!!!

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ function makevalues(list,meow){
         <tr style="width: 100%">
           <td>Value ${i+1}</td>
           <td>Level ${formatWhole(meow[i])}/10 (*${new Decimal(2).pow(meow[i])})</td>
-          <td id="m_valueamount${i}"></td>
+          <td><span id="m_valueamount${i}"></span></td>
           <td><button id="m_buybutton${i}" onclick="console.log('meow')">${format(new Decimal(10).pow(i+1))}</button></td>
         </tr><br>`
      
@@ -26,6 +26,9 @@ function setupvalues(){
    if (lastvaluelength != player.m_values.length){
       gel("m_values").innerHTML = makevalues(player.m_values,player.m_valuebuys)
       lastvaluelength = player.m_values.length
+      for (let i = 0; i < lastvaluelength; i++){
+      gel(`m_buybutton${i}`).onclick = () => {console.log('meow')}
+   }
    }
    for (let i = 0; i < lastvaluelength; i++){
       gel(`m_valueamount${i}`).textContent = format(player.m_values[i])
@@ -38,4 +41,5 @@ setInterval(() => {
     player.m_number = player.m_number.add(player.m_values[0].divide(tickspersecond).times(new Decimal(2).pow(player.m_valuebuys[0])))
     gel("m_number").textContent = format(player.m_number,6)
     gel("m_values").innerHTML = makevalues(player.m_values,player.m_valuebuys)
+    setupvalues()
 }, 1000/tickspersecond);
