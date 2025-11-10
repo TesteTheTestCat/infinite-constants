de0987222a7b09c718f495b753de63998be1b795
Teste <silly>
2025-11-10 19:39:29 -0300

    hmm....

--- formatting.js
+++ formatting.js
@@ -53,7 +53,7 @@ export function formatWhole(decimal, reallyWhole=false) {
 	decimal = new Decimal(decimal)
 	if (decimal.gte(1e9)) return format(decimal, 2)
 	if (decimal.lte(0.95) && !decimal.eq(0) && !reallyWhole) return format(decimal, 2)
-	else return format(decimal, 0, true)
+	else return format(decimal, 0, false)
 }
 function formatTime(s) {
 	s = new Decimal(s);
