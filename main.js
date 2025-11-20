caa30f2b0f794cac642b444c7ef6d1a1a43b1815
Teste <silly>
2025-11-20 12:10:07 -0300

    i love deltatime~ meow meow~

--- main.js
+++ main.js
@@ -2,11 +2,13 @@ import {format, formatWhole} from "./formatting.js";
 import {valuecost} from "./helper.js"
 let player = {
     version: "alpha0.03",
+    lasttick: Date.now(),
     m_number: new Decimal(10),
     m_values: [new Decimal(0),new Decimal(0),new Decimal(0),new Decimal(0)],
     m_valuebuys: [new Decimal(0),new Decimal(0),new Decimal(0),new Decimal(0)]
 }
 let lastvaluelength = 0
+const maxticks = 10000 //the maximum amount of ticks before ticksize increases
 const gel = (name) => document.getElementById(name)
 const tickspersecond = 20
 function makevalues(list,meow){
@@ -46,10 +48,16 @@ function buyvalue(i){
   }
 }
 setInterval(() => {
+   let deltatime = Date.now() - player.lasttick //straight up just deltatime
+   let ticksize = 1/tickspersecond
+   if (deltatime/tickspersecond >= maxticks) {ticksize = deltatime/maxticks}
+   while (deltatime >= ticksize){
     for(let i = 0; i < player.m_values.length-1; i++){
-       player.m_values[i] = player.m_values[i].add(player.m_values[i+1].divide(tickspersecond).times(new Decimal(2).pow(player.m_valuebuys[i+1])))
+       player.m_values[i] = player.m_values[i].add(player.m_values[i+1].divide(ticksize).times(new Decimal(2).pow(player.m_valuebuys[i+1])))
     }
-    player.m_number = player.m_number.add(player.m_values[0].divide(tickspersecond).times(new Decimal(2).pow(player.m_valuebuys[0])))
+    player.m_number = player.m_number.add(player.m_values[0].divide(ticksize).times(new Decimal(2).pow(player.m_valuebuys[0])))
+    deltatime -= ticksize
+   }
     gel("m_number").textContent = format(player.m_number,8)
     gel("m_numberps").textContent = format(player.m_values[0].times(new Decimal(2).pow(player.m_valuebuys[0])),6)+"/s"
     setupvalues()
