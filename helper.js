97509679280a1edf91fc12b276dde487ef50a3c7
Teste <silly>
2025-12-15 19:05:12 -0300

    nyaaaa meow meow :3

--- helper.js
+++ helper.js
@@ -14,7 +14,7 @@ export function catspacesoftcat(c){
   if (c.lt(1e100)){
     return 1 //-root
   }
-  return c.log10().minus(100).pow(2) //-root
+  return c.log10().minus(100).pow(2).plus(1) //-root
 }
 export function constructvalues(len){
     let list = []
